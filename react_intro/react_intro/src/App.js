b055394a2a21018b1b4e92159e7d4d753b7fc8f0
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-08-24 08:38:22 +0200

    changed some text

--- react_intro/react_intro/src/App.js
+++ react_intro/react_intro/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dette er react :)
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jeg vet hvor du bor
         </a>
       </header>
     </div>
