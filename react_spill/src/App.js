81d39194cc2f142549bd13ad8098cc0e2377f8ed
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-09-05 14:06:01 +0200

    working on tetris

--- react_spill/src/App.js
+++ react_spill/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import GridBoard from './components/GridBoard';
+import NextBlock from './components/NextBlock';
+import ScoreBoard from './components/ScoreBoard';
+
+import React from 'react';
+import { createStore } from 'redux'
+import reducers from './reducers'
+
+const store = createStore(reducers)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Gaming</h1>
       </header>
+      <GridBoard props="1" />
+      <NextBlock />
+      <ScoreBoard />
     </div>
   );
 }
