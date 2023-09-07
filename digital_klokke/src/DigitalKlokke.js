9af424828c87e583cafafb68b114376f39eae582
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-09-07 10:27:40 +0200

    confetti change

--- digital_klokke/src/DigitalKlokke.js
+++ digital_klokke/src/DigitalKlokke.js
@@ -3,27 +3,26 @@ import ConfettiExplosion from 'react-confetti-explosion';
 
 export function DigitalClock() {
 
-
     const [currentTime, setTime] = useState("")
     const [isExploding, setIsExploding] = useState(false);
 
     useEffect(() => {
-
+        setIsExploding(true)
         const clockUpdate = setInterval(() => {
             let clock = new Date()
             setTime(clock.getHours() + ":" + clock.getMinutes() + ":" + clock.getSeconds())
-            setIsExploding(true)
-        }, 500);
+            setIsExploding(false)
+        }, 1000);
         return () => clearInterval(clockUpdate)
     })
-
-
-
 
     return (
         <>
             <h1> {JSON.stringify(currentTime)}   </h1>
-            {isExploding && <ConfettiExplosion />}
+            {isExploding && <ConfettiExplosion
+                particleSize={10}
+                force={1}
+            />}
         </>
     )
 }
