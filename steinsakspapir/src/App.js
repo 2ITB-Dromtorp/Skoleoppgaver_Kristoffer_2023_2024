f94e1bca68f745d487e80c6dbbb2e08f7a22d74e
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-09-13 13:22:59 +0200

    ؤرلالاءؤ额外分发ferdig mနd saks s‌ေks papirမါတန‌ျေိလခبلقثسؤ的说法阿

--- steinsakspapir/src/App.js
+++ steinsakspapir/src/App.js
@@ -34,37 +34,60 @@ function Battle() {
   }
   else if (Player1 === "Papir") {
     if (Player2 === "Papir") {
-
+      return "Tie"
     }
     if (Player2 === "Saks") {
-
+      return "Lose"
     }
     if (Player2 === "Stein") {
-
+      return "Win"
     }
   }
 }
 
 function chooseWeapon(weapon) {
-  Player1 = weapon
+  Player1 = weapon.target.value
   Player2 = enemyRandom()
+  document.getElementById("P1Image").src = require("./" + Player1 + ".jpg")
+  document.getElementById("P2Image").src = require("./" + Player2 + ".jpg")
+  switch (Battle()) {
+    case "Win":
+      document.getElementById("status").innerHTML = "Player vant"
+      break;
+    case "Lose":
+      document.getElementById("status").innerHTML = "Slem robot vant"
+      break;
+    case "Tie":
+      document.getElementById("status").innerHTML = "Ingen vant"
+      break;
+    default:
+      document.getElementById("status").innerHTML = "bruh"
+      break;
+  }
 }
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Stein Saks papir </h1>
+
         <div id="Battlefield">
+
           <div id="Player1">
-            <h1>Player 1</h1>
-            <img src="stein.jpg" id="P1Image" alt="p1"></img>
+            <h1>Player</h1>
+            <img src="" id="P1Image" alt="p1"></img>
           </div>
           <div id="Player2">
-            <h1>Player 2</h1>
-            <img src="./stein.jpg" id="P2Image" alt="p2"></img>
+            <h1>Slem Robot</h1>
+            <img src="" id="P2Image" alt="p2"></img>
           </div>
         </div>
+
+        <h3 id="status"></h3>
 
         <div id="Buttons">
           <button value="Stein" onClick={(value) => chooseWeapon(value)}>Stein</button>
