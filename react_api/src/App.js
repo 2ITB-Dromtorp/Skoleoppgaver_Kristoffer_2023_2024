34793a49b3712c721ca21a2d27b84af6e65da6ab
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-09-14 10:46:02 +0200

    ်ငလနညငါ့အျအ်ေမနီပျ2 nye projecter教大家南非将你看女人您

---
+++ react_api/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+async function jokeAPI() {
+  const response = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart");
+  const movies = await response.json();
+  document.getElementById("Setup").innerHTML = JSON.stringify(movies.setup)
+  document.getElementById("Delivery").innerHTML = JSON.stringify(movies.delivery)
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={() => jokeAPI()}>Press for Joke</button>
+        <div id="Setup"></div>
+        <div id="Delivery"></div>
+      </header>
+    </div >
+  );
+}
+
+export default App;
---
+++ react_spill2/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Canvas from './Canvas.js'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Episk spill </h1>
+        <Canvas />
+      </header>
+    </div>
+  );
+}
+
+export default App;
