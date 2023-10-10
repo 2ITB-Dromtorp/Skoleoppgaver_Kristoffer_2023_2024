6f6298d288ad6c104c47506fa4c92866c4a41ff6
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2023-10-10 13:48:05 +0200

    خاةغلخي شةلاشعفخنشةအ့န သညန စငနခန ငျ မန‌ေူthe one piece is real阿米巴u通卡吗亖्ौहोल्ोता ्गलुता

--- klasse_kart/src/Profile.js
+++ klasse_kart/src/Profile.js
@@ -1,23 +1,26 @@
 import { useNavigate, useParams } from 'react-router-dom';
 const elever = require('./elever.json');
 
+export default function Profile() {
 
+    const navigate = useNavigate();
 
-export function checkifUserExist() {
+    let userId = useParams();
 
     let eleverListe = JSON.parse(JSON.stringify(elever))
 
-    for (let i = 0; i < eleverListe.Elever.length; i++) {
-        if (eleverListe.Elever[i].Fornavn.toLowerCase() === userId.profile) {
+    for (let i = 0; i < eleverListe.length; i++) {
+        if (eleverListe[i].Fornavn.toLowerCase() === userId.profile) {
             return (
                 <>
                     <div className='elev-container'>
-                        <h1> Dette er profilen til {userId.profile} </h1>
+                        <h1> Dette er profilen til {eleverListe[i].Fornavn} {eleverListe[i].Etternavn} </h1>
+                        <h2> Email: {eleverListe[i].Email} </h2>
                         <button onClick={() => navigate(-1)}> Tilbake til hovedmeny </button>
                     </div>
                 </>
             )
-        } else {
+        } else if (i === (eleverListe.length - 1)) {
             console.log("feil bruekr")
             return (
                 <p> denne brukeren fins ikke </p>
@@ -26,19 +29,4 @@ export function checkifUserExist() {
     }
 
 
-}
-
-
-export default function Profile() {
-
-    const navigate = useNavigate();
-
-    let userId = useParams();
-
-    return (
-        <>
-            <checkifUserExist />
-        </>
-
-    )
 }
