a358c71fb66bcf50dfef95dfa07b9d3c2d4f9f41
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-01-23 13:31:49 +0100

    sykt mekking

---
+++ Firebase/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='Container'>
+      <h1>Username</h1>
+      <input type='text'></input>
+      <h1>Password</h1>
+      <input type='text'></input>
+    </div>
+  );
+}
+
+export default App;
