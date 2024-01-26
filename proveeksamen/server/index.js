7c045f1ea7e2bd26bcab78222aa408ffd863f44d
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-01-26 09:26:24 +0100

    גדעקקרשגה

--- proveeksamen/server/index.js
+++ proveeksamen/server/index.js
@@ -4,10 +4,14 @@ var cors = require("cors");
 //const sqlite3 = require('sqlite3').verbose();
 //const db = new sqlite3.Database("./database.db");
 const app = express()
-//const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 
 app.use(cors());
 
 app.use(express.static("build"))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
