bcc0da3778b20fa05b80f96e0bde51717bd3b9cc
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-01-26 09:25:02 +0100

    fix

---
+++ proveeksamen/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var cors = require("cors");
+//const bcrypt = require("bcrypt")
+//const sqlite3 = require('sqlite3').verbose();
+//const db = new sqlite3.Database("./database.db");
+const app = express()
+//const port = process.env.PORT || 8080
+
+app.use(express.json())
+
+app.use(cors());
+
+app.use(express.static("build"))
