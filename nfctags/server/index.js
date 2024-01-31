1b2b72fd7ad95ee1a8b3f42010836704d29c02c4
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-01-31 12:26:55 +0100

    jeg er føler meg faktisk sigma idag nullll cap :)

---
+++ nfctags/server/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("build"))
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    app.get("/get", (req, res) => {
+        res.status(200).json({ "message": "👌" })
+    })
+
+    app.get("*", (req, res) => {
+        res.sendFile("build")
+    })
+})
