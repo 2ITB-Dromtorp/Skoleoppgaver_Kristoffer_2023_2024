5cec47c5aeba963284666558f5461c5fd81c92c9
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-02-29 11:51:35 +0100

    adding cors to mulighet

--- nfcsnakeladders/server/index.js
+++ nfcsnakeladders/server/index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080", // Specify the exact origin
+        origin: ("http://mulighet.no:" + process.env.PORT), // Specify the exact origin
         methods: ["GET", "POST"],
         credentials: true
     }
