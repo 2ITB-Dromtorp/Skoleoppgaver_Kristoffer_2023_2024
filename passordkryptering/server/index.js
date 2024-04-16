c3d7130597c5dec4580373b9f459fd1f4fd2dced
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-04-16 14:23:15 +0200

    trying to setup

--- passordkryptering/server/index.js
+++ passordkryptering/server/index.js
@@ -1,12 +1,37 @@
 const express = require('express')
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
 const app = express()
-const port = 3001
+const port = process.env.PORT || 8080
 var cors = require("cors");
 app.use(express.json())
 
+const url = process.env.TEST
+
 app.use(cors())
 
-function Hash(input) {
+app.listen(port, () => {
+
+  const mongodb = new MongoClient(url);
+
+  const database = mongodb.db("Database");
+  const Users = database.collection("Teacher");
+  console.log(Users)
+  console.log(`Example app listening on port ${port}`)
+  
+  app.get('/get', async (req, res) => {
+    try {
+      const users = await Users.find().toArray();
+      res.send(users);
+    } catch (error) {
+      console.error("Error fetching documents:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  })
+  
+})
+
+/*function Hash(input) {
   let hash = 0;
   if (input.length === 0) return hash;
   for (let i = 0; i < input.length; i++) {
@@ -15,13 +40,6 @@ function Hash(input) {
       hash &= hash;
   }
   return hash;
-}
+}*/
 
-app.post('/hash', async (req, res) => {
-  const { Password } = await req.body
-  res.send(Hash(Password).toString())
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
